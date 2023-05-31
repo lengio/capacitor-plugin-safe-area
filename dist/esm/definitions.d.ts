d559300499afc415c329d2c6bfc938bf49a46282
Kamran Khan <kamran@kage.com>
2023-05-31 14:35:00 -0400

    As a band-aid fix, add dist directory to the repo to allow for local builds

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,30 @@
+import type { PluginListenerHandle } from '@capacitor/core';
+export interface SafeAreaPlugin {
+    /**
+     *  get mobile SafeArea info
+     */
+    getSafeAreaInsets(): Promise<SafeAreaInsets>;
+    /**
+     * get mobile statusbar height
+     */
+    getStatusBarHeight(): Promise<StatusBarInfo>;
+    /**
+     * event listener when safe-area changed
+     * @param event
+     * @param listenerFunc
+     */
+    addListener(event: 'safeAreaChanged', listenerFunc: (data: SafeAreaInsets) => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+}
+interface SafeArea {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+}
+export interface SafeAreaInsets {
+    insets: SafeArea;
+}
+export interface StatusBarInfo {
+    statusBarHeight: number;
+}
+export {};
